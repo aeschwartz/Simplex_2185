d20221a69409877eec8c57a06d08ff2f215ca79d
Andrew Schwartz <andrewschwartz99@gmail.com>
2019-02-27 16:06:25 -0500

    finished E08

--- E08 - Projections/AppClass.cpp
+++ E08 - Projections/AppClass.cpp
@@ -3,7 +3,7 @@ using namespace Simplex;
 void Application::InitVariables(void)
 {
 	//Change this to your name and email
-	m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
+	m_sProgrammer = "Andrew Schwartz - aes7176@rit.edu";
 
 	//Set the position and target of the camera
 	//(I'm at [0,0,10], looking at [0,0,0] and up is the positive Y axis)
@@ -54,21 +54,46 @@ void Application::Display(void)
 		break;
 	case 2:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPerspective(false);
 		break;
 	case 3:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(
+			vector3(30.0f, 0.0f, 0.0f),
+			vector3(1.0f, 0.0f, 0.0f),
+			vector3(0.0f, 0.0f, -1.0f)
+		);
 		break;
 	case 4:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(
+			vector3(0.0f, 0.0f, -15.0f),
+			vector3(0.0f, 0.0f, -1.0f),
+			vector3(0.0f, 1.0f, 0.0f)
+		);
 		break;
 	case 5:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(
+			vector3(0.0f, 0.0f, -15.0f),
+			vector3(0.0f, 0.0f, -1.0f),
+			vector3(0.0f, 1.0f, 0.0f)
+		);
+		m_pCamera->SetNearFar(vector2(6.0f, 1000.0f));
 		break;
 	case 6:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(
+			vector3(0.0f, 0.0f, -15.0f),
+			vector3(0.0f, 0.0f, -1.0f),
+			vector3(0.0f, 1.0f, 0.0f)
+		);
+		m_pCamera->SetNearFar(vector2(0.001f, 10.0f));
+		break;
 		break;
 	case 7:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetFOV(-45.0f);
 		break;
 	}
 
